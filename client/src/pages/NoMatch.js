98c70ee17273cc7d82d10f97e9dc466985bdfd61
Sydney Dunlap <sydney.dunlap2011@gmail.com>
2019-03-26 15:47:31 -0400

    more edits

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -2,21 +2,18 @@ import React from "react";
 import Col from "../components/Col";
 import Row from "../components/Row";
 import Container from "../components/Container";
-import Jumbotron from "../components/Jumbotron";
 
 function NoMatch() {
   return (
     <Container fluid>
       <Row>
         <Col size="md-12">
-          <Jumbotron>
             <h1 className="text-center">404 Page Not Found</h1>
             <h1 className="text-center">
               <span role="img" aria-label="Face With Rolling Eyes Emoji">
                 🙄
               </span>
             </h1>
-          </Jumbotron>
         </Col>
       </Row>
     </Container>
