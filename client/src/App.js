41a056d9bc9cb8c5289804bacf0ec3a1574aad37
Sydney Dunlap <sydney.dunlap2011@gmail.com>
2019-03-26 15:39:50 -0400

    fixing errors in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Saved from "./pages/Discover";
+import Saved from "./pages/Saved";
 import Search from "./pages/Search";
 import NoMatch from "./pages/NoMatch";
 import Navbar from "./components/Navbar";
