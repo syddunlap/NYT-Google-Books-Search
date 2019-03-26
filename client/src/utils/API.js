7bb8d549000c9aeaba84a7c38f942632bd39206e
Sydney Dunlap <sydney.dunlap2011@gmail.com>
2019-03-26 14:32:41 -0400

    api calls in utils added

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,23 @@
-// Make calls to Google Books API from here.
+import axios from "axios";
 
-// Example from Pupster:
+// Export an object containing methods we'll use for accessing the Google & Books API
 
-// import axios from "axios";
-
-// Export an object containing methods we'll use for accessing the Dog.Ceo API
-
-// export default {
-//   getRandomDog: function() {
-//     return axios.get("https://dog.ceo/api/breeds/image/random");
-//   },
-//   getDogsOfBreed: function(breed) {
-//     return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
-//   },
-//   getBaseBreedsList: function() {
-//     return axios.get("https://dog.ceo/api/breeds/list");
-//   }
-// };
+export default {
+    // Gets books from the Google API
+    getBooks: function(q) {
+        return axios.get("/api/google", { params: { q: "title:" + q}});
+    },
+    // Get all saved books from database
+    getSavedBooks: function() {
+        return axios.get("/api/books");
+    },
+    // Delete a saved book using the id
+    deleteBook: function() {
+        return axios.delete("api/books/" + id);
+    },
+    // Save a book to the database
+    saveBook: function() {
+        return axios.post("api/books", bookData);
+    }
+};
 
